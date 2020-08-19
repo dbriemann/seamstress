1b111cc233a566674545a4c8cec6f6bc2662e072
David Linus Briemann <dlb@mailbox.org>
2020-08-19 17:16:15 +0200

    add filechooser dialog

--- main.go
+++ main.go
@@ -1,27 +1,39 @@
 package main
 
 import (
-	"os"
+	"fmt"
 
-	"github.com/gotk3/gotk3/glib"
 	"github.com/gotk3/gotk3/gtk"
 )
 
-func main() {
-	// Create Gtk Application, change appID to your application domain name reversed.
-	const appID = "io.github.dbriemann"
-	application, err := gtk.ApplicationNew(appID, glib.APPLICATION_FLAGS_NONE)
-	failOn(err)
+var (
+	mainWin *gtk.Window
+)
 
-	// Application signals available
-	// startup -> sets up the application when it first starts
-	// activate -> shows the default first window of the application (like a new document). This corresponds to the application being launched by the desktop environment.
-	// open -> opens files and shows them in a new window. This corresponds to someone trying to open a document (or documents) using the application from the file browser, or similar.
-	// shutdown ->  performs shutdown tasks
-	// Setup Gtk Application callback signals
-	application.Connect("activate", func() { onActivate(application) })
-	// Run Gtk application
-	os.Exit(application.Run(os.Args))
+func main() {
+	gtk.Init(nil)
+
+	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	failOn(err)
+	mainWin = win
+	mainWin.SetTitle("Seamstress")
+	mainWin.Connect("destroy", func() {
+		gtk.MainQuit()
+	})
+
+	mainLayout, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 2)
+	failOn(err)
+	mainWin.Add(mainLayout)
+
+	menu := makeMainMenu()
+	mainLayout.PackStart(menu, false, false, 0)
+
+	content := makeMainContent()
+	mainLayout.PackStart(content, true, true, 0)
+
+	mainWin.SetDefaultSize(400, 600)
+	mainWin.ShowAll()
+	gtk.Main()
 }
 
 func makeMenu(root string, children ...string) *gtk.MenuItem {
@@ -35,15 +47,18 @@ func makeMenu(root string, children ...string) *gtk.MenuItem {
 		if c != "" {
 			item, err := gtk.MenuItemNewWithLabel(c)
 			failOn(err)
-			menu.Add(item)
+			item.Connect("activate", openImage)
+			menu.Append(item)
 		} else {
 			item, err := gtk.SeparatorMenuItemNew()
 			failOn(err)
-			menu.Add(item)
+			menu.Append(item)
 		}
 	}
 	return rootItem
 }
+
+// TODO -- create struct: name,action menu item
 
 func makeMainMenu() *gtk.MenuBar {
 	mainMenu, err := gtk.MenuBarNew()
@@ -51,13 +66,13 @@ func makeMainMenu() *gtk.MenuBar {
 
 	// File..
 	fileMenu := makeMenu("File", "Open", "Save", "", "Quit")
-	mainMenu.Add(fileMenu)
+	mainMenu.Append(fileMenu)
 	// Edit..
 	editMenu := makeMenu("Edit", "Mark", "Cut")
-	mainMenu.Add(editMenu)
+	mainMenu.Append(editMenu)
 	// Help..
 	helpMenu := makeMenu("Help", "About")
-	mainMenu.Add(helpMenu)
+	mainMenu.Append(helpMenu)
 
 	return mainMenu
 }
@@ -74,11 +89,30 @@ func makeMainButton(iconName string) *gtk.Button {
 	return btn
 }
 
+func openImage() {
+	dialog, err := gtk.FileChooserNativeDialogNew("Open Image", mainWin, gtk.FILE_CHOOSER_ACTION_OPEN, "_Open", "_Cancel")
+	failOn(err)
+	filter, err := gtk.FileFilterNew()
+	failOn(err)
+	filter.AddPattern("*.png")
+	filter.AddPattern("*.jpg")
+	filter.AddPattern("*.jpeg")
+	dialog.AddFilter(filter)
+	resp := dialog.Run()
+	if gtk.ResponseType(resp) == gtk.RESPONSE_ACCEPT {
+		chooser := dialog
+		fname := chooser.GetFilename()
+		fmt.Println(fname)
+		// TODO
+	}
+}
+
 func makeMainContent() *gtk.Grid {
 	grid, err := gtk.GridNew()
 	failOn(err)
 
 	openBtn := makeMainButton("gtk-open")
+	openBtn.Connect("clicked", openImage)
 	saveBtn := makeMainButton("gtk-save")
 	markBtn := makeMainButton("gtk-edit")
 	cutBtn := makeMainButton("gtk-cut")
@@ -92,36 +126,6 @@ func makeMainContent() *gtk.Grid {
 	grid.Attach(undoBtn, 1, 3, 1, 1)
 	grid.Attach(redoBtn, 2, 3, 1, 1)
 	return grid
-}
-
-func newWindow(application *gtk.Application) *gtk.ApplicationWindow {
-	// Create ApplicationWindow
-	win, err := gtk.ApplicationWindowNew(application)
-	failOn(err)
-
-	// Set ApplicationWindow Properties
-	win.SetTitle("Seamstress")
-
-	mainLayout, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 2)
-	failOn(err)
-	win.Add(mainLayout)
-
-	menu := makeMainMenu()
-	mainLayout.PackStart(menu, false, false, 0)
-
-	content := makeMainContent()
-	mainLayout.PackStart(content, true, true, 0)
-
-	win.SetDefaultSize(400, 600)
-
-	return win
-}
-
-// Callback signal from Gtk Application
-func onActivate(application *gtk.Application) {
-	win := newWindow(application)
-
-	win.ShowAll()
 }
 
 func failOn(err error) {
